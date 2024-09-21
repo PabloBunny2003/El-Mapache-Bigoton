6772a805e8d1c657245501e1dd65b4196a5f0cc0
Felipe García <L21TE0645@teziutlan.tecnm.mx>
2024-09-20 21:57:13 -0600

    Ventana principal
    Creación de ventana principal

--- Mapache Bigoton/Principal.cs
+++ Mapache Bigoton/Principal.cs
@@ -16,5 +16,20 @@ namespace Mapache_Bigoton
         {
             InitializeComponent();
         }
+
+        private void btnAgregarCita_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Salida_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
