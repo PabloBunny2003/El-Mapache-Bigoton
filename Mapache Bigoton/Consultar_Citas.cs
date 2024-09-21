3ce37442be76b1c44c400a66420a79cf5a9c9ebc
Pablo Manuel San Pedro Baltazar <baltazarpablo1997@gmail.com>
2024-09-20 23:17:17 -0600

    Formulario Consultar citas
    Se desarrollo el formulario de consultar_citas

--- Mapache Bigoton/Consultar_Citas.cs
+++ Mapache Bigoton/Consultar_Citas.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,60 @@ namespace Mapache_Bigoton
         public Consultar_Citas()
         {
             InitializeComponent();
+        }
+
+        private void Consultar_Citas_Load(object sender, EventArgs e)
+        {
+            //Agregamos un método donde hace toda la operación de cargar los datos
+            //desde la base de datos al dataGridView
+            CargarDatos();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Crear una instancia para pasar al formulario Principal (Redirigirnos)
+            Principal formularioPrincipal = new Principal();
+            // Aquí mostramos el formulario
+            formularioPrincipal.Show();
+            this.Close();
+        }
+
+        private void CargarDatos()
+        {
+            // Hacemos nuestra cadena de conexión a la base de datos
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["BarberiaBD"].ConnectionString;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT Nombre, Apellido, Telefono, Servicio, FechaHora FROM Citas";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // Agregamos una nueva fila al DataGridView con los datos leídos
+                                // a la hora de agregar una cita
+                                dataGridView1.Rows.Add(
+                                    reader["Nombre"].ToString(),
+                                    reader["Apellido"].ToString(),
+                                    reader["Telefono"].ToString(),
+                                    reader["Servicio"].ToString(),
+                                    Convert.ToDateTime(reader["FechaHora"])
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Ocurrió un error al cargar los datos: " + error.Message);
+            }
         }
     }
 }
