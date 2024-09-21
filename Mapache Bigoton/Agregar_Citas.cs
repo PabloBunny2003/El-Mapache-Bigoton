8b12eb911e878343ff59961215aebb421e2212b5
Pablo Manuel San Pedro Baltazar <baltazarpablo1997@gmail.com>
2024-09-20 20:39:44 -0600

    Inicio de proyecto
    Creacion de los formularios del proyecto

---
+++ Mapache Bigoton/Agregar_Citas.cs
@@ -0,0 +1,10 @@
+namespace Mapache_Bigoton
+{
+    public partial class Agregar_Citas : Form
+    {
+        public Agregar_Citas()
+        {
+            InitializeComponent();
+        }
+    }
+}
